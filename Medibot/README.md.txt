84dad96a589af25232114d08db2ba28ae5c3113e
shabbar47 <60538788+shabbar47@users.noreply.github.com>
2021-06-06 00:45:42 +0530

    Medibot final version Code Base

---
+++ Medibot/README.md.txt
@@ -0,0 +1,2 @@
+• Medi bot checks the temperature and pulse rate of the patient whose information is stored in the database of the bot. It works on a motor mechanism which dispenses the medicine prescribed to the patient by the doctor.
+• In this bot, the facial recognition feature is also inbuilt to help Identify the patient and correspondingly dispatch the medicine to the right patient to avoid errors.
---
+++ Medibot/statusTracker.ino
@@ -0,0 +1,79 @@
+#include <LiquidCrystal.h>
+#include <Servo.h> 
+ 
+Servo myservo1,myservo2,myservo3,myservo4;
+LiquidCrystal lcd(12, 13,8,9,10,11);
+                
+int value=0,tPin=1;
+void setup() 
+{ 
+  lcd.begin(16, 2);
+  lcd.print("Morning Dose!!");
+  myservo1.attach(1);
+  myservo2.attach(2);
+  myservo3.attach(3);
+  myservo4.attach(4);
+} 
+ 
+void loop() 
+{  
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Place your");
+  lcd.setCursor(0, 1);
+  lcd.print("Finger (Temp)!!");
+  value=analogRead(tPin);
+  int cel= (value/1024)*500;
+  int far= (cel*9)/5 +32;
+  delay(5000);
+  lcd.clear();
+  
+  lcd.setCursor(0, 0);
+  lcd.print("Your Temp is :");
+  lcd.setCursor(7, 1);
+  lcd.print(far);
+  lcd.setCursor(10, 0);
+  lcd.print("Farhnt");
+  delay(3000);
+  lcd.clear();
+  
+  
+  lcd.setCursor(0, 0);
+  lcd.print("Place your");
+  lcd.setCursor(0, 1);
+  lcd.print("Finger (BPM)!!");
+  delay(2000);
+  lcd.clear();
+  
+  
+  lcd.setCursor(0, 0);
+  lcd.print("Calculating BPM ♥");
+  lcd.setCursor(4, 1);
+  lcd.print("♥♥♥ ....");
+  delay(10000);
+  lcd.clear();
+  
+  lcd.setCursor(0, 0);
+  lcd.print("Take Your Tabs!");
+  
+  
+  myservo1.write(0);
+  delay(5000);
+  myservo1.write(90);
+  delay(5000);
+  myservo2.write(0);
+  delay(5000);
+  myservo2.write(90);
+  delay(5000);
+  myservo3.write(0);
+  delay(5000);
+  myservo3.write(90);
+  delay(5000);
+  if(far>100)
+  {
+    myservo4.write(0);
+    delay(5000);
+    myservo4.write(90);
+    delay(5000);
+  }
+}
